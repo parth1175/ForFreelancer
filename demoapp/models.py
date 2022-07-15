593419f589c0ff803f8946c7bd791ee011747eaf
rohan-chitrao <rohanchitrao@gmail.com>
2022-07-15 00:55:29 -0400

    5th commit

--- demoapp/models.py
+++ demoapp/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class data(models.Model):
     user = models.CharField(max_length=100, default = '1')
-    url = models.CharField(max_length=100, default = 'www.error.com')
-    Company = models.CharField(max_length=100, default = 'ApplyAway')
-    Job = models.CharField(max_length=100, default = 'Error')
-    Description = models.CharField(max_length=10000, default = 'Great Company')
+    url = models.CharField(max_length=1000, default = 'www.error.com')
+    Company = models.CharField(max_length=1000, default = 'ApplyAway')
+    Job = models.CharField(max_length=1000, default = 'Error')
+    Description = models.CharField(max_length=100000, default = 'Great Company')
     Notes =  models.CharField(max_length=10000, default = ' ')
