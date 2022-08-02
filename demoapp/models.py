9d5e931fd6e2373f6a476e2bf25a51aad11f9a8e
rohan-chitrao <rohanchitrao@gmail.com>
2022-08-02 14:13:16 -0400

    Default values changed

--- demoapp/models.py
+++ demoapp/models.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 
 # Create your models here.
 class data(models.Model):
-    user = models.CharField(max_length=100, default = 'missing')
-    Website = models.CharField(max_length=100, default = 'missing')
-    Url = models.CharField(max_length=1000, default = 'missing')
-    Company = models.CharField(max_length=1000, default = 'missing')
-    Job = models.CharField(max_length=1000, default = 'missing')
-    Description = models.CharField(max_length=100000, default = 'missing')
+    user = models.CharField(max_length=100, default = 'Missing')
+    Website = models.CharField(max_length=100, default = 'Missing')
+    Url = models.CharField(max_length=1000, default = 'Missing')
+    Company = models.CharField(max_length=1000, default = 'Missing')
+    Job = models.CharField(max_length=1000, default = 'Missing')
+    Description = models.CharField(max_length=100000, default = 'Missing')
     Notes =  models.CharField(max_length=10000, default = '')
-    Applied = models.CharField(max_length=1000, default='missing')
+    Applied = models.CharField(max_length=1000, default='Missing')
     Date = models.DateTimeField(default=timezone.now)
 
